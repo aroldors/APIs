3536467b89412d4d3bc8ff92a7e9f6f906ee5a10
Aroldo Silva <aroldomxm@gmail.com>
2024-10-25 16:52:22 -0300

    Refatoração dos métodos de extração do cnpj e empresa

--- DecriptPfx/CertificadoService.cs
+++ DecriptPfx/CertificadoService.cs
@@ -2,21 +2,28 @@ using System.Security.Cryptography.X509Certificates;
 
 public class CertificadoService
 {
+    public string? cnpj;
+    
     public CertificadoInfo DescriptografarCertificado(byte[] certificadoBytes, string senha)
-    {
+    {        
         // Carregar o certificado PFX usando a senha fornecida
         var cert = new X509Certificate2(certificadoBytes, senha, X509KeyStorageFlags.MachineKeySet);
+        var subject = cert.Subject;
 
         // Obter o período de validade do certificado
         var validadeInicial = cert.NotBefore;
         var validadeFinal = cert.NotAfter;
 
-        // Extrair o CNPJ da empresa a partir do campo Subject
-        var cnpj = cert.Subject.Split(',').FirstOrDefault(x => x.Trim().StartsWith("2.5.4.45="))?.Replace("2.5.4.45=", "").Trim();
-
         // Extrair o nome da empresa
-        var nomeEmpresa = cert.Subject.Split(',').FirstOrDefault(x => x.Trim().StartsWith("CN="))?.Replace("CN=", "").Trim();
+        //var nomeEmpresa = cert.Subject.Split(',').FirstOrDefault(x => x.Trim().StartsWith("CN="))?.Replace("CN=", "").Trim();
+        var nomeEmpresa = ExtractCompanyName(subject);
 
+        if(!string.IsNullOrWhiteSpace(subject))
+        {
+            // Extrair o CNPJ da empresa a partir do campo nomeEmpresa
+            cnpj = ExtractCnpj(subject);
+        }      
+        
         // Retornar as informações encapsuladas
         return new CertificadoInfo
         {
@@ -26,4 +33,45 @@ public class CertificadoService
             ValidadeFinal = validadeFinal
         };
     }
+
+    private string? ExtractCnpj(string subject)
+    {
+        var cnpjPrefix = ":";
+        var cnpjStartIndex = subject.IndexOf(cnpjPrefix);
+        if (cnpjStartIndex >= 0)
+        {
+            cnpjStartIndex += cnpjPrefix.Length;
+            var cnpjEndIndex = subject.IndexOf(',', cnpjStartIndex);
+            if (cnpjEndIndex == -1)
+            {
+                cnpjEndIndex = subject.Length;
+            }
+
+            var cnpj = subject.Substring(cnpjStartIndex, cnpjEndIndex - cnpjStartIndex);
+            if (cnpj.All(char.IsDigit) && cnpj.Length == 14)
+            {
+                return cnpj;
+            }
+        }
+        return null;
+    }
+
+    private string? ExtractCompanyName(string subject)
+    {
+        var companyNamePrefix = "CN=";
+        var companyNameStartIndex = subject.IndexOf(companyNamePrefix);
+        if (companyNameStartIndex >= 0)
+        {
+            companyNameStartIndex += companyNamePrefix.Length;
+            var companyNameEndIndex = subject.IndexOf(':', companyNameStartIndex);
+            if (companyNameEndIndex == -1)
+            {
+                companyNameEndIndex = subject.Length;
+            }
+
+            return subject.Substring(companyNameStartIndex, companyNameEndIndex - companyNameStartIndex);
+        }
+        return null;
+    }
+
 }
